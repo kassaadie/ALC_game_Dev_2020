005ea909635ea670569407bcabb1de8c510c54f2
kassadie barney <kassadie.b161@stu.nebo.edu>
2019-09-18 15:15:42 -0600

    add unity project

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      trasform.posistion=  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
